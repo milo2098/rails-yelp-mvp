876df5283e21bdcef9abcf9708bbeb34a39bcbff
Milo Vingoe <milovingoe@hotmail.com>
2025-05-21 15:29:20 +0100

    Restaurant and Review Model created, and restaurant routing controller and view created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning the database..."
+Restaurant.destroy_all
+
+nandos = { name: "Nando's", address: "10 Chicken Street", phone_number: "0123712389", category: "french" }
+pizza_hut = { name: "Pizza Hut", address: "11 Pizza Street", phone_number: "6234986848", category: "italian" }
+yo_sushi = { name: "Yo Sushi", address: "12 Sushi Street", phone_number: "9328476588", category: "japanese" }
+uncle_wrinkle = { name: "Uncle Wrinkle", address: "13 Chinese Street", phone_number: "1384746783", category: "chinese" }
+waffles = { name: "Wafflemeister", address: "14 Waffle Street", phone_number: "0987234564", category: "belgian" }
+
+
+puts "creating restaurants..."
+
+[nandos, pizza_hut, yo_sushi, uncle_wrinkle, waffles].each do |restaurant|
+  puts "creating #{restaurant}"
+  Restaurant.create!(restaurant)
+end
+
+puts "Finished, created #{Restaurant.count} restaurants."
